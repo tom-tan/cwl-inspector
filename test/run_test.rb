de0d05c1d52ab430863191855b23387d477885e5
Tomoya Tanjo <ttanjo@gmail.com>
2017-09-11 17:41:47 +0900

    Add tests

---
+++ test/run_test.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+if $0 == __FILE__
+  base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+  lib_dir  = File.join(base_dir, "lib")
+  test_dir = File.join(base_dir, "test")
+
+  $LOAD_PATH.unshift(lib_dir)
+
+  require 'test/unit'
+
+  exit Test::Unit::AutoRunner.run(true, test_dir)
+end
---
+++ test/test-echo.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+require 'test/unit'
+require_relative '../cwl-inspector'
+
+CWL_PATH=File.join(File.dirname(__FILE__), '..', 'examples')
+
+class TestEcho < Test::Unit::TestCase
+  def test_version
+    assert_equal(cwl_inspect("#{CWL_PATH}/echo.cwl", 'cwlVersion'),
+                 'v1.0')
+  end
+
+  def test_commandline
+    assert_equal(cwl_inspect("#{CWL_PATH}/echo.cwl", 'commandline'),
+                'docker run --rm docker/whalesay cowsay [ $input ]')
+  end
+end
