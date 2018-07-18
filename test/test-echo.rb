cdf64b865f14f2fe3def41387b00ed5aa5fc82c2
Tomoya Tanjo <ttanjo@gmail.com>
2018-07-18 14:49:58 +0900

    Fix environment dependent paths

--- test/test-echo.rb
+++ test/test-echo.rb
@@ -16,6 +16,14 @@ class TestEcho < Test::Unit::TestCase
       'docdir' => '.',
       'tmpdir' => '/tmp',
     }
+    @vardir = case RUBY_PLATFORM
+              when /darwin|mac os/
+                '/private/var'
+              when /linux/
+                '/var'
+              else
+                raise "Unsupported platform: #{RUBY_PLATFORM}"
+              end
   end
 
   def test_version
@@ -31,12 +39,12 @@ class TestEcho < Test::Unit::TestCase
   end
 
   def test_commandline
-    assert_equal("docker run -i --read-only --rm --workdir=/private/var/spool/cwl --env=HOME=/private/var/spool/cwl --env=TMPDIR=/tmp --user=#{Process::UID.eid}:#{Process::GID.eid} -v #{Dir.pwd}:/private/var/spool/cwl -v /tmp:/tmp docker/whalesay cowsay > #{Dir.pwd}/output",
+    assert_equal("docker run -i --read-only --rm --workdir=#{@vardir}/spool/cwl --env=HOME=#{@vardir}/spool/cwl --env=TMPDIR=/tmp --user=#{Process::UID.eid}:#{Process::GID.eid} -v #{Dir.pwd}:#{@vardir}/spool/cwl -v /tmp:/tmp docker/whalesay cowsay > #{Dir.pwd}/output",
                  commandline(@cwl, @runtime, parse_inputs(@cwl, {}, @runtime)))
   end
 
   def test_instantiated_commandline
-    assert_equal("docker run -i --read-only --rm --workdir=/private/var/spool/cwl --env=HOME=/private/var/spool/cwl --env=TMPDIR=/tmp --user=#{Process::UID.eid}:#{Process::GID.eid} -v #{Dir.pwd}:/private/var/spool/cwl -v /tmp:/tmp docker/whalesay cowsay \'Hello!\' > #{Dir.pwd}/output",
+    assert_equal("docker run -i --read-only --rm --workdir=#{@vardir}/spool/cwl --env=HOME=#{@vardir}/spool/cwl --env=TMPDIR=/tmp --user=#{Process::UID.eid}:#{Process::GID.eid} -v #{Dir.pwd}:#{@vardir}/spool/cwl -v /tmp:/tmp docker/whalesay cowsay \'Hello!\' > #{Dir.pwd}/output",
                  commandline(@cwl, @runtime,
                              parse_inputs(@cwl, { 'input' => 'Hello!' }, @runtime)))
   end
--- test/test-workflow.rb
+++ test/test-workflow.rb
@@ -17,11 +17,19 @@ class TestWorkflow < Test::Unit::TestCase
       'docdir' => [@cwldir],
       'tmpdir' => '/tmp',
     }
+    @vardir = case RUBY_PLATFORM
+              when /darwin|mac os/
+                '/private/var'
+              when /linux/
+                '/var'
+              else
+                raise "Unsupported platform: #{RUBY_PLATFORM}"
+              end
   end
 
   def test_arguments
     cwlfile = File.join(@cwldir, 'arguments.cwl')
-    assert_equal("docker run -i --read-only --rm --workdir=/private/var/spool/cwl --env=HOME=/private/var/spool/cwl --env=TMPDIR=/tmp --user=#{Process::UID.eid}:#{Process::GID.eid} -v #{Dir.pwd}/tmp:/private/var/spool/cwl -v /tmp:/tmp -v #{File.expand_path @cwldir}/Foo.java:/private/var/lib/cwl/inputs/Foo.java:ro java:7-jdk javac '-d' '#{Dir.pwd}/tmp' '/private/var/lib/cwl/inputs/Foo.java'",
+    assert_equal("docker run -i --read-only --rm --workdir=#{@vardir}/spool/cwl --env=HOME=#{@vardir}/spool/cwl --env=TMPDIR=/tmp --user=#{Process::UID.eid}:#{Process::GID.eid} -v #{Dir.pwd}/tmp:#{@vardir}/spool/cwl -v /tmp:/tmp -v #{File.expand_path @cwldir}/Foo.java:#{@vardir}/lib/cwl/inputs/Foo.java:ro java:7-jdk javac '-d' '#{Dir.pwd}/tmp' '#{@vardir}/lib/cwl/inputs/Foo.java'",
                  commandline(cwlfile,
                              @runtime,
                              parse_inputs(cwlfile,
