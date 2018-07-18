578bf1943cb219d764412b0e1031b45189c3419a
Tomoya Tanjo <ttanjo@gmail.com>
2018-07-18 15:19:36 +0900

    [Travis] Use `deploy` field instead of `after_success` field

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_OS_NAME" = "linux" ]; then
-    echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-    docker build -t $DOCKER_USER/cwl-inspector:latest .
-    docker push $DOCKER_USER/cwl-inspector:latest
-fi
+echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+docker build -t $DOCKER_USER/cwl-inspector:latest .
+docker push $DOCKER_USER/cwl-inspector:latest
