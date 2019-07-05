c3e68c44ccc43b24a0d8da257a402ca71ff583ec
Tomoya Tanjo <tanjo@acsioma.jp>
2019-07-05 17:39:14 +0900

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tag=${$TRAVIS_TAG:-latest}
+tag=${TRAVIS_TAG:-latest}
 
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker build -t $DOCKER_USER/cwl-inspector:$tag .
