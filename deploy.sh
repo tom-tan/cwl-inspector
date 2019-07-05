e9cada8f9602bdd98ec0c15d89eed1b7ce38226f
Tomoya Tanjo <tanjo@acsioma.jp>
2019-07-05 17:35:34 +0900

    Update deploy.sh to use git tags

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+tag=${$TRAVIS_TAG:-latest}
+
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-docker build -t $DOCKER_USER/cwl-inspector:latest .
-docker push $DOCKER_USER/cwl-inspector:latest
+docker build -t $DOCKER_USER/cwl-inspector:$tag .
+docker push $DOCKER_USER/cwl-inspector:$tag
